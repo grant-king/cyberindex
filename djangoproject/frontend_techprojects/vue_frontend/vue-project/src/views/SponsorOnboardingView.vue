8f85edfe97f6888b0b095df471576b50c346b567
Grant King <grantryan.king@gmail.com>
2025-01-13 13:25:27 -0800

    Create SponsorOnboardingView.vue

---
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/views/SponsorOnboardingView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <OnboardingForm />
+    </div>
+    <div>
+        <OnboardingReview />
+    </div>
+</template>
+
+<script setup>
+import OnboardingForm from '@/components/OnboardingForm.vue';
+import OnboardingReview from '@/components/OnboardingReview.vue';
+</script>
