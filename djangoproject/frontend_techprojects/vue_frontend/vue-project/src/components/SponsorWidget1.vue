342380a21153fdb3df906fbfe2faa4ab33a07c30
Grant King <grantryan.king@gmail.com>
2025-01-12 21:34:07 -0800

    Create SponsorWidget1.vue

---
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/SponsorWidget1.vue
@@ -0,0 +1,26 @@
+<template>
+    <img class="h-40 w-auto" :src="logo_source" alt="" />
+    <figure class="mt-10">
+        <blockquote class="text-lg/8 font-semibold text-black">
+            <p>“{{ message }}”</p>
+        </blockquote>
+        <figcaption class="mt-10 flex gap-x-6">
+            <img :src="sponsor_avatar" alt="" class="size-12 flex-none rounded-full bg-gray-50" />
+            <div>
+                <div class="text-base font-semibold text-black">{{ author }}</div>
+                <div class="text-sm/6 text-black">{{ sponsor_role }} for {{ org_name }}</div>
+            </div>
+        </figcaption>
+    </figure>
+</template>
+
+<script setup>
+const props = defineProps({
+    message: String,
+    author: String,
+    sponsor_role: String,
+    org_name: String,
+    sponsor_avatar: String,
+    logo_source: String
+});
+</script>
