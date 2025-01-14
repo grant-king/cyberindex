445c38d938f349fd559f98c47f6eef5777507cec
Grant King <grantryan.king@gmail.com>
2025-01-14 11:32:11 -0800

    Register sponsors models in admin

--- djangoproject/sponsors/admin.py
+++ djangoproject/sponsors/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Sponsor, SponsorProfile, SponsorMessage, SponsorContribution
 
 # Register your models here.
+admin.site.register(Sponsor)
+admin.site.register(SponsorProfile)
+admin.site.register(SponsorMessage)
+admin.site.register(SponsorContribution)
