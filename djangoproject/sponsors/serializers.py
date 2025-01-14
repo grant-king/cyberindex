7a9d537a777bbb55514b06595801e8764a603d73
Grant King <grantryan.king@gmail.com>
2025-01-14 11:27:28 -0800

    Add serializers for sponsors models

--- djangoproject/sponsors/serializers.py
+++ djangoproject/sponsors/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate
 from rest_framework import serializers
+from .models import Sponsor, SponsorProfile, SponsorMessage, SponsorContribution
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
@@ -11,3 +12,26 @@ class LoginSerializer(serializers.Serializer):
             return user
         raise serializers.ValidationError("Incorrect Credentials")
     
+class SponsorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sponsor
+        fields = ['id', 'user', 'archived']
+
+    
+class SponsorProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SponsorProfile
+        fields = ['rep_name', 'rep_role', 'rep_image_url', 'company_name', 'company_logo_url', 'company_website']
+
+
+class SponsorMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SponsorMessage
+        fields = ['message', 'created_at']
+    
+
+class SponsorContributionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SponsorContribution
+        fields = ['amount', 'created_at']
+        
