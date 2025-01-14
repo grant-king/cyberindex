86d8eae7b566ba6108a26e92c699e1da9f709708
Grant King <grantryan.king@gmail.com>
2025-01-14 14:32:31 -0800

    Update OnboardingForm.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/OnboardingForm.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/OnboardingForm.vue
@@ -19,67 +19,8 @@
             <h2 class="text-pretty text-4xl font-semibold tracking-tight text-black sm:text-5xl">{{ title_1 }}</h2>
             <p class="mt-2 text-lg/8 text-black">{{ subtitle_1 }}</p>
             <div class="mt-16 flex flex-col gap-16 sm:gap-y-20 lg:flex-row">
-                <form action="#" method="POST" class="lg:flex-auto">
-                    <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                        <div>
-                            <label for="first-name" class="block text-sm/6 font-semibold text-black">First name</label>
-                            <div class="mt-2.5">
-                                <input type="text" name="first-name" id="first-name" autocomplete="given-name"
-                                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-black/50 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
-                            </div>
-                        </div>
-                        <div>
-                            <label for="last-name" class="block text-sm/6 font-semibold text-black">Last name</label>
-                            <div class="mt-2.5">
-                                <input type="text" name="last-name" id="last-name" autocomplete="family-name"
-                                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-black/50 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
-                            </div>
-                        </div>
-                        <div>
-                            <label for="org_name" class="block text-sm/6 font-semibold text-black">Organization
-                                Name</label>
-                            <div class="mt-2.5">
-                                <input id="org_name" name="org_name" type="text"
-                                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-black/50 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
-                            </div>
-                        </div>
-                        <div>
-                            <label for="org_role" class="block text-sm/6 font-semibold text-black">Your role</label>
-                            <div class="mt-2.5">
-                                <input type="url" name="org_role" id="org_role"
-                                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-black/50 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
-                            </div>
-                        </div>
-                        <div class="sm:col-span-2">
-                            <label for="website" class="block text-sm/6 font-semibold text-black">Website</label>
-                            <div class="mt-2.5">
-                                <input type="url" name="website" id="website"
-                                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-black/50 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
-                            </div>
-                        </div>
-                        <div class="sm:col-span-2">
-                            <label for="logo_url" class="block text-sm/6 font-semibold text-black">Organization logo URL</label>
-                            <div class="mt-2.5">
-                                <input type="url" name="logo_url" id="logo_url"
-                                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-black/50 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
-                            </div>
-                        </div>
-                        <div class="sm:col-span-2">
-                            <label for="message" class="block text-sm/6 font-semibold text-black">Public Message</label>
-                            <div class="mt-2.5">
-                                <textarea id="message" name="message" rows="4"
-                                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-black/50 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mt-10">
-                        <button type="button"
-                            class="block w-full rounded-md bg-lime-200 px-3.5 py-2.5 text-center text-sm font-bold text-white/20 bg-clip-text border-4 border-dotted border-black shadow-sm hover:bg-lime-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600">Create
-                            Sponsorship Profile</button>
-                    </div>
-                    <p class="mt-4 text-sm/6 text-black">By submitting this form, I agree to the <a href="#"
-                            class="font-semibold hover:text-lime-950">privacy&nbsp;policy</a>.</p>
-                </form>
+                <SponsorProfileCreate />
+                <SponsorMessageCreate />
                 <!-- Sponsorship Example -->
                 <div class="lg:mt-6 lg:w-80 lg:flex-none">
                     <SponsorWidget1 :message="testimonial" :author="testimonial_author" 
@@ -93,6 +34,8 @@
 
 <script setup>
 import SponsorWidget1 from './SponsorWidget1.vue';
+import SponsorMessageCreate from './SponsorMessageCreate.vue';
+import SponsorProfileCreate from './SponsorProfileCreate.vue';
 
 const title_1 = 'Let’s get your sponsorship details in order.'
 const subtitle_1 = 'This information will be used to create your sponsorship profile.'
