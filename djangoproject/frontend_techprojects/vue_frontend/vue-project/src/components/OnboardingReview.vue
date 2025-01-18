bba0b070d998f8429c69a75a8faa448a57ad2683
Grant King <grantryan.king@gmail.com>
2025-01-18 06:49:25 -0800

    Update OnboardingReview.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/OnboardingReview.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/OnboardingReview.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="bg-white/50">
         <div class="border-black border-dashed border-y-8">
-            <SponsorWidget3 :message="sponsormessages_store.new_message_preview.message" :author="preview.rep_name" :sponsor_role="preview.rep_role"
-                :org_name="preview.company_name" :sponsor_avatar="preview.rep_image_url"
-                :company_logo_url="preview.company_logo_url" />
+            <SponsorWidget3 :message="sponsormessages_store.new_message_preview.message" :author="sponsorprofiles_store.new_object_preview.rep_name" :sponsor_role="sponsorprofiles_store.new_object_preview.rep_role"
+                :org_name="sponsorprofiles_store.new_object_preview.company_name" :sponsor_avatar="sponsorprofiles_store.new_object_preview.rep_image_url"
+                :company_logo_url="sponsorprofiles_store.new_object_preview.company_logo_url" />
         </div>
         <div class="mt-16 pb-32 mx-auto max-w-xl lg:max-w-4xl">
             <div class="flex flex-col gap-16 sm:gap-y-20 lg:flex-row">
                 <div class="lg:flex-auto">
-                    <SponsorWidget1 :message="sponsormessages_store.random_message.message" :author="preview.rep_name"
-                        :sponsor_role="preview.rep_role" :org_name="preview.company_name"
-                        :sponsor_avatar="preview.rep_image_url" :company_logo_url="preview.company_logo_url" />
+                    <SponsorWidget1 :message="sponsormessages_store.get_random_message().message" :author="sponsorprofiles_store.my_profile_data.rep_name"
+                        :sponsor_role="sponsorprofiles_store.my_profile_data.rep_role" :org_name="sponsorprofiles_store.my_profile_data.company_name"
+                        :sponsor_avatar="sponsorprofiles_store.my_profile_data.rep_image_url" :company_logo_url="sponsorprofiles_store.my_profile_data.company_logo_url" />
                 </div>
                 <div class="lg:flex-auto">
-                    <SponsorWidget2 :message="sponsormessages_store.new_message_preview.message" :author="preview.rep_name"
-                        :sponsor_role="preview.rep_role" :org_name="preview.company_name"
-                        :sponsor_avatar="preview.rep_image_url" :company_logo_url="preview.company_logo_url" />
+                    <SponsorWidget2 :message="sponsormessages_store.get_random_message().message" :author="sponsorprofiles_store.my_profile_data.rep_name"
+                        :sponsor_role="sponsorprofiles_store.my_profile_data.rep_role" :org_name="sponsorprofiles_store.my_profile_data.company_name"
+                        :sponsor_avatar="sponsorprofiles_store.my_profile_data.rep_image_url" :company_logo_url="sponsorprofiles_store.my_profile_data.company_logo_url" />
                 </div>
             </div>
         </div>
@@ -30,8 +30,6 @@ import SponsorWidget2 from './SponsorWidget2.vue';
 import SponsorWidget3 from './SponsorWidget3.vue';
 
 const sponsorprofiles_store = useSponsorprofilesStore();
-const preview = sponsorprofiles_store.new_object_preview;
-
 const sponsormessages_store = useSponsormessagesStore();
 set_preview_message();
 
