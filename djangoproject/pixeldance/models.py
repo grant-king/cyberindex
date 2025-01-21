c0df7970c4fff9ae506694b12c77d5b4ceeb1e9f
Grant King <grantryan.king@gmail.com>
2025-01-21 06:35:04 -0800

    Add initial pixeldance models

--- djangoproject/pixeldance/models.py
+++ djangoproject/pixeldance/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Dancer(models.Model):
+    color = models.CharField(max_length=7, db_index=True)
+    session_id = models.CharField(max_length=32, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+class DancePath(models.Model):
+    dancer = models.ForeignKey(Dancer, on_delete=models.CASCADE, related_name='paths')
+    start_x = models.PositiveSmallIntegerField(db_index=True)
+    start_y = models.PositiveSmallIntegerField(db_index=True)
+    end_x = models.PositiveSmallIntegerField(db_index=True)
+    end_y = models.PositiveSmallIntegerField(db_index=True)
+    delta_x = models.SmallIntegerField(db_index=True)
+    delta_y = models.SmallIntegerField(db_index=True)
+    event_timestamp = models.PositiveIntegerField()
+    click_time_delta = models.IntegerField()
+
