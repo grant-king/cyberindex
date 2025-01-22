270610ba357f8da935f39e3989bf07dbc144259e
Grant King <grantryan.king@gmail.com>
2025-01-21 17:39:42 -0800

    Update pixeldance store for persisting to DRF DB

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/stores/pixeldance.js
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/stores/pixeldance.js
@@ -2,9 +2,55 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const usePixeldanceStore = defineStore('pixeldance', () => {
-    const pixeldance_list = ref([])
+    const pixeldance_list = ref([]) // for animating newly recorded and fetched pixeldances
+    const base_url = window.location.origin
+    const endpoint = `${base_url}/apiv1/dancers/`
+    const next_page = ref(`${endpoint}?page=1`)
+    const path_endpoint = `${base_url}/apiv1/dancepaths/`
+    const my_dancer_list = ref([]) // dancers with detail endpoint urls for fetching related dance paths
+    const new_dancer_preview = ref({}) // next dancer object to be created
+    const new_path_preview = ref({}) // next dance path object to be created
 
-    function store_pixel_dance(dance_paths, color) {
+    async function createDancer() {
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': window.csrf_token,
+            },
+            body: JSON.stringify(new_dancer_preview.value),
+        })
+        if (response.ok) {
+            const data = await response.json()
+            my_dancer_list.value.push(data)
+            console.log('Dancer created successfully')
+            return data
+        } else {
+            console.error('Failed to create dancer')
+            console.error(response)
+        }
+    }
+
+    async function createDancePath() {
+        const response = await fetch(path_endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': window.csrf_token,
+            },
+            body: JSON.stringify(new_path_preview.value),
+        })
+        if (response.ok) {
+            const data = await response.json()
+            console.log('Dance path created successfully')
+            return data
+        } else {
+            console.error('Failed to create dance path')
+            console.error(response)
+        }
+    }
+
+    async function store_pixel_dance(dance_paths, color) {
         const dance = {}
         dance.idx = pixeldance_list.value.length
         dance.color = color
@@ -17,6 +63,29 @@ export const usePixeldanceStore = defineStore('pixeldance', () => {
 
         pixeldance_list.value.push(dance)
         console.log(pixeldance_list.value)
+
+        new_dancer_preview.value = {
+            color: color,
+        }
+
+        const dancer = await createDancer()
+        if (dancer) {
+            console.log(dancer)
+            for (let i = 0; i < dance_paths.length; i++) {
+                new_path_preview.value = {
+                    dancer: dancer.url,
+                    start_x: Math.floor(dance_paths[i].start_x),
+                    start_y: Math.floor(dance_paths[i].start_y),
+                    end_x: Math.floor(dance_paths[i].end_x),
+                    end_y: Math.floor(dance_paths[i].end_y),
+                    delta_x: Math.floor(dance_paths[i].delta_x),
+                    delta_y: Math.floor(dance_paths[i].delta_y),
+                    event_timestamp: Math.floor(dance_paths[i].event_time),
+                    click_time_delta: Math.floor(dance_paths[i].click_time_delta),
+                }
+                await createDancePath()
+            }
+        }
     }
 
     function step(timestamp, dance) {
@@ -24,13 +93,13 @@ export const usePixeldanceStore = defineStore('pixeldance', () => {
             dance.start_timestamp = timestamp
         }
         const elapsed_time = timestamp - dance.start_timestamp
-        
-        const shift_x = (dance.paths[dance.current_path].delta_x / 2000 ) * elapsed_time
-        const shift_y = (dance.paths[dance.current_path].delta_y / 2000 ) * elapsed_time
+
+        const shift_x = (dance.paths[dance.current_path].delta_x / 2000) * elapsed_time
+        const shift_y = (dance.paths[dance.current_path].delta_y / 2000) * elapsed_time
 
         dance.x = dance.paths[dance.current_path].start_x + shift_x
         dance.y = dance.paths[dance.current_path].start_y + shift_y
-        
+
         //animate each path according to durations btw events as recorded
         if (elapsed_time < dance.paths[dance.current_path].click_time_delta) {
             window.requestAnimationFrame((timestamp) => step(timestamp, dance))
@@ -55,8 +124,8 @@ export const usePixeldanceStore = defineStore('pixeldance', () => {
         animate_next_path(dance)
     }
 
-    function animate_all_dances(){
-        for (let i=0; i < pixeldance_list.value.length; i++) {
+    function animate_all_dances() {
+        for (let i = 0; i < pixeldance_list.value.length; i++) {
             animate_dance(i)
         }
     }
