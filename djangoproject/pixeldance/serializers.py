35b9d5d31dd46f5e0ed3586469338f624673d636
Grant King <grantryan.king@gmail.com>
2025-01-21 17:36:47 -0800

    Update DancePathSerializer

--- djangoproject/pixeldance/serializers.py
+++ djangoproject/pixeldance/serializers.py
@@ -8,9 +8,9 @@ class DancerSerializer(serializers.HyperlinkedModelSerializer):
         read_only_fields = ['session_id']
 
 
-class DancePathSerializer(serializers.ModelSerializer):
+class DancePathSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = DancePath
-        fields = ['id', 'dancer', 'start_x', 'start_y', 'end_x', 'end_y', 'delta_x', 'delta_y', 'event_timestamp', 'click_time_delta']
+        fields = ['url', 'dancer', 'start_x', 'start_y', 'end_x', 'end_y', 'delta_x', 'delta_y', 'event_timestamp', 'click_time_delta']
 
     
