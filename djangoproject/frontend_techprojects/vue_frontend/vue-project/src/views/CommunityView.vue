310c4f209e1592a81646518aa941a6c1f1822bcf
Grant King <grantryan.king@gmail.com>
2025-01-22 22:43:01 -0800

    Update CommunityView.vue
    Add BasicHeader

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/views/CommunityView.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/views/CommunityView.vue
@@ -1,8 +1,11 @@
 <template>
+    <BasicHeader eyebrow="you are so very welcome" title="Community Center"
+        description="Or 'Chill Zone'? 'Meditation Lagoon'? Feel free to give it a private nickname. Whatever you want to call this place, please have a good time." />
     <PixelDanceArt />
 </template>
 
 <script setup>
 import PixelDanceArt from '@/components/PixelDanceArt.vue';
+import BasicHeader from '@/components/BasicHeader.vue';
 
 </script>
