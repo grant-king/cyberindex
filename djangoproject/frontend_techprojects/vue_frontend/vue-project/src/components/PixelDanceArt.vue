fb7be11391c5b26e1ecf116e223f4b4c5f0bc855
Grant King <grantryan.king@gmail.com>
2025-01-23 04:09:20 -0800

    Update PixelDanceArt.vue
    Update styles

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/PixelDanceArt.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/PixelDanceArt.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="flex flex-col align-center gap-1 bg-white/10">
-        <div class="relative m-auto border-2 bg-black/95"
-            :style="{ width: `${stage_width}px`, height: `${stage_height}px` }" @click="play_pixel_dance">
-            <div v-for="pixeldance in pixeldance_store.pixeldance_list" :key="pixeldance.id"
-                class="absolute w-4 h-4 opacity-80 rounded-full"
-                :style="{ left: `${pixeldance.x * (stage_width / 400)}px`, top: `${pixeldance.y * (stage_width / 400)}px`, backgroundColor: pixeldance.color}">
+    <div class="">
+        <div class="flex mx-auto align-center justify-center bg-white/50 overflow-hidden">
+            <div class="border-4 border-dotted">
+                <div class="relative bg-black"
+                    :style="{ width: `${stage_width}px`, height: `${stage_height}px` }" @click="play_pixel_dance">
+                    <div v-for="pixeldance in pixeldance_store.pixeldance_list" :key="pixeldance.id"
+                        class="absolute w-4 h-4 opacity-80 rounded-full"
+                        :style="{ left: `${pixeldance.x * (stage_width / 400)}px`, top: `${pixeldance.y * (stage_width / 400)}px`, backgroundColor: pixeldance.color }">
 
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -25,7 +29,7 @@ async function play_pixel_dance() {
     //set stage height and width to device width
     stage_width.value = Math.min(window.innerHeight, window.innerWidth)
     stage_height.value = Math.min(window.innerHeight, window.innerWidth)
-    
+
     await pixeldance_store.load_communal_dances_page()
     pixeldance_store.animate_all_dances()
 }
