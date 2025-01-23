e599cd1a38f3b9e8745b901040443848fec4bf4a
Grant King <grantryan.king@gmail.com>
2025-01-23 04:09:53 -0800

    Update CommunityView.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/views/CommunityView.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/views/CommunityView.vue
@@ -2,10 +2,12 @@
     <BasicHeader eyebrow="you are so very welcome" title="Community Center"
         description="Or 'Chill Zone'? 'Meditation Lagoon'? Feel free to give it a private nickname. Whatever you want to call this place, please have a good time." />
     <PixelDanceArt />
+    <SponsorCarousel />
 </template>
 
 <script setup>
 import PixelDanceArt from '@/components/PixelDanceArt.vue';
 import BasicHeader from '@/components/BasicHeader.vue';
+import SponsorCarousel from '@/components/SponsorCarousel.vue';
 
 </script>
