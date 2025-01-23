c50c1fda6c9299045ae651fe49e76bc2d4158b5d
Grant King <grantryan.king@gmail.com>
2025-01-23 04:09:36 -0800

    Create SponsorCarousel.vue

---
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/SponsorCarousel.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="bg-white/50 mx-auto">
+    <div class="flex items-center justify-center">
+        <div class="max-w-xl"
+        @click="store.readMeditation">
+            <SponsorWidget2 
+            :message="store.current_preview.message"
+            :author="store.current_preview.rep_name"
+            :sponsor_role="store.current_preview.rep_role"
+            :org_name="store.current_preview.company_name"
+            :sponsor_avatar="store.current_preview.rep_image_url"
+            :company_logo_url="store.current_preview.company_logo_url"
+            />
+        </div>
+    </div>
+</div>
+</template>
+
+<script setup>
+import { useMeditationsStore } from '@/stores/sponsormeditations';
+import SponsorWidget2 from './SponsorWidget2.vue';
+import { ref } from 'vue'
+
+const store = useMeditationsStore();
+console.log(store.current_preview)
+
+</script>
