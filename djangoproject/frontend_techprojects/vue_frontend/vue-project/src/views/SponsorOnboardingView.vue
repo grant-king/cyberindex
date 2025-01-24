4f0fee2ab02bcfaa6ce5a0165576ff858d0f68ae
Grant King <grantryan.king@gmail.com>
2025-01-23 17:51:43 -0800

    Update SponsorOnboardingView.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/views/SponsorOnboardingView.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/views/SponsorOnboardingView.vue
@@ -5,11 +5,15 @@
     <div>
         <OnboardingReview />
     </div>
+    <div>
+        <OnboardingCheckout />
+    </div>
 </template>
 
 <script setup>
 import OnboardingForm from '@/components/OnboardingForm.vue';
 import OnboardingReview from '@/components/OnboardingReview.vue';
+import OnboardingCheckout from '@/components/OnboardingCheckout.vue';
 
 import { useSponsorprofilesStore } from '@/stores/sponsorprofiles';
 
