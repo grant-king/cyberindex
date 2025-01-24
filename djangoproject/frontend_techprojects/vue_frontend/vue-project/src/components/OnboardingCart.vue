09781b3e5730a434a93f1bdc30c6c4ee2ce2f58c
Grant King <grantryan.king@gmail.com>
2025-01-24 02:18:39 -0800

    Create OnboardingCart.vue

---
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/OnboardingCart.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="py-8">
+        <div class="mx-auto max-w-7xl text-center">
+            <ul role="list" class="mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2">
+                <li v-for="(item, idx) in packages" :key="idx" 
+                :class="[item.selected ? 'rounded-2xl bg-indigo-200/20 ring-2 ring-white/80 ring-offset-4' : 'rounded-2xl bg-indigo-200/5 ']"
+                @click="select_item(idx)">
+                <div 
+                    class="py-10">
+                    <img class="mx-auto size-48 rounded-full" :src="item.imageUrl" alt="" />
+                    <h3 class="mt-6 text-xl font-semibold tracking-tight text-white">{{ item.amount }}</h3>
+                    <p class="text-sm/6 text-black/80">{{ item.subtext }}</p>
+                </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const selected_item = ref(null)
+
+function select_item(item_idx) {
+    for (let item = 0; item < packages.value.length; item++) {
+        packages.value[item].selected = false
+        if (item == item_idx){
+            packages.value[item].selected = true
+        }
+    }
+}
+
+const packages = ref([
+    {
+        selected: false,
+        amount: '$40',
+        subtext: 'small package',
+        imageUrl:
+            'https://unrolla.blob.core.windows.net/unrolla/images/P8220536.webp',
+    },
+    {
+        active: false,
+        amount: '$200',
+        subtext: 'big package',
+        imageUrl:
+            'https://unrolla.blob.core.windows.net/unrolla/images/P6080370.webp',
+    },
+
+])
+</script>
