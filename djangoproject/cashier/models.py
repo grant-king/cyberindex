23bc6a8957eb058404a6d0958c14499a68ac4f47
Grant King <grantryan.king@gmail.com>
2025-01-24 18:37:32 -0800

    Update models.py

--- djangoproject/cashier/models.py
+++ djangoproject/cashier/models.py
@@ -14,3 +14,25 @@ class StripePayment(models.Model):
     amount_pennies = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     session_key = models.CharField(max_length=40)
+
+
+class Order(models.Model):
+    payment = models.OneToOneField(StripePayment, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    completed_at = models.DateTimeField(blank=True, null=True)
+
+
+class FundsCredit(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="credits"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    amount_pennies = models.PositiveIntegerField()
+
+
+class FundsDebit(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="credits"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    amount_pennies = models.PositiveIntegerField()
