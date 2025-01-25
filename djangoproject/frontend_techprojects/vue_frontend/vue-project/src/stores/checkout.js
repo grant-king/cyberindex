e7e18f03a948248fea2cb4989ee444a213a66f79
Grant King <grantryan.king@gmail.com>
2025-01-25 01:05:00 -0800

    Update checkout.js

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/stores/checkout.js
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/stores/checkout.js
@@ -2,24 +2,27 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCheckoutStore = defineStore('checkout', () => {
-  async function submitCheckout(amount_pennies) {
-    const response = await fetch('/apiv1/checkout/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRFToken': window.csrf_token,
-      },
-      body: JSON.stringify({ amount_pennies }),
-    })
-    if (response.ok) {
-      const data = await response.json()
-      console.log(data)
-      return data
-    } else {
-      console.error('failed to submit checkout')
-      console.error(response)
-    }
-  }
+    const current_payment = ref({'amount_pennies': 0 })
 
-  return { submitCheckout }
+    async function submitCheckout(amount_pennies) {
+        const response = await fetch('/apiv1/checkout/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': window.csrf_token,
+            },
+            body: JSON.stringify({ amount_pennies }),
+        })
+        if (response.ok) {
+            const data = await response.json()
+            current_payment.value = data
+            console.log(current_payment.value)
+            return data
+        } else {
+            console.error('failed to submit checkout')
+            console.error(response)
+        }
+    }
+
+    return { submitCheckout, current_payment }
 })
