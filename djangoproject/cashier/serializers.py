87f28ec9940cdc8b8bdec7cb73ea1460b38a52fa
Grant King <grantryan.king@gmail.com>
2025-01-24 18:42:37 -0800

    Update serializers.py

--- djangoproject/cashier/serializers.py
+++ djangoproject/cashier/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from cashier.models import StripePayment, Customer
+from cashier.models import StripePayment, Customer, Order, FundsCredit, FundsDebit
 
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -14,3 +14,25 @@ class StripePaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = StripePayment
         fields = ["intent", "customer", "amount_pennies", "created_at", "session_key"]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Order
+        fields = ["payment", "customer", "completed_at"]
+
+
+class FundsCreditSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = FundsCredit
+        fields = ["customer", "created_at", "amount_pennies"]
+
+
+class FundsDebitSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = FundsDebit
+        fields = ["customer", "created_at", "amount_pennies"]
+        
