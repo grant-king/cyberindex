85e12e0f6cbfcc72601f04ecbfe8f96814eefd05
Grant King <grantryan.king@gmail.com>
2025-01-25 02:15:20 -0800

    Update OnboardingPayment.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/OnboardingPayment.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/OnboardingPayment.vue
@@ -9,9 +9,17 @@
                     <!-- main stripe payment element will be inserted here -->
                 </div>
                 <div>
-                    <button id="submit-button" class="btn btn-primary"
-                    @click="submit_payment">
-                        Submit Payment
+                    <button class="uppercase mt-8" @click="submit_payment">
+                        <div
+                            class="px-4 py-2 border-4 hover:bg-clip-border hover:bg-lime-200 hover:bg-white/20 hover:text-white/50 transition-all duration-1000 border-white/50 border-dotted rounded-lg">
+                            <div
+                                class="transition-all duration-1000 text-white/20 hover:text-black/80 hover:bg-lime-400 bg-lime-200 bg-clip-text font-extrabold">
+                                <div class="font-mono text-4xl">
+                                    Pay ${{ store.current_payment.intent.amount / 100 }} {{
+                                        store.current_payment.intent.currency }}
+                                </div>
+                            </div>
+                        </div>
                     </button>
                 </div>
             </form>
@@ -35,13 +43,32 @@ function submit_payment() {
 }
 
 async function update_stripe_elements() {
+    const appearance = {
+        theme: 'flat',
+    }
+    const options = {
+        layout: {
+            type: 'accordion',
+            defaultCollapsed: false,
+            radios: false,
+            spacedAccordionItems: true,
+        }
+    }
     const public_key = store.current_payment.intent.public_key
-    console.log('public_key', public_key)
+    const client_key = store.current_payment.intent.client_secret
+    stripe.value = await loadStripe(public_key)
+    elements.value = stripe.value.elements({
+        appearance,
+        clientSecret: client_key,
+    })
+    const payment_element = elements.value.create('payment', options)
+    payment_element.mount('#payment-element')
+    const link_auth_element = elements.value.create('linkAuthentication')
+    link_auth_element.mount('#link-authentication-element')
 }
 
 // watch for changes to the current payment intent, and update stripe elements
 watch(() => store.current_payment, async (newval, oldval) => {
-    //console.log('current_payment changed', oldval, newval)
     await update_stripe_elements()
 })
 </script>
