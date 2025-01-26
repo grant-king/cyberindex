d815abbc11b8cc5dd4f3360966749d760ef979c2
Grant King <grantryan.king@gmail.com>
2025-01-25 20:41:28 -0800

    Update checkout.js

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/stores/checkout.js
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/stores/checkout.js
@@ -11,6 +11,8 @@ export const useCheckoutStore = defineStore('checkout', () => {
     const show_pay_button = ref(false)
     const show_shop = ref(true)
     const credit_balance = ref(0)
+    const customer_endpoint = ref('')
+    const customer_user = ref(0)
 
     async function submitCheckout(amount_pennies) {
         const response = await fetch('/apiv1/checkout/', {
@@ -52,5 +54,29 @@ export const useCheckoutStore = defineStore('checkout', () => {
         }
     }
 
-    return { submitCheckout, confirmOrderStatus, current_payment, show_pay_button }
+    async function getCreditBalance(){
+        const response = await fetch('/apiv1/customers/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': window.csrf_token,
+            }
+        })
+        if (response.ok) {
+            const data = await response.json()
+            console.log(data)
+            credit_balance.value = data.results[0].credit_balance
+            customer_endpoint.value = data.results[0].url
+            customer_user.value = data.results[0].user
+            if (credit_balance.value > 3800){
+                show_shop.value = false
+            }
+            return data
+        } else {
+            console.error('failed to get credit balance')
+            console.error(response)
+        }
+    }
+
+    return { submitCheckout, confirmOrderStatus, getCreditBalance, current_payment, show_pay_button, show_shop, credit_balance, customer_endpoint, customer_user }
 })
