99b6875f2ccf86699c9216268728c47f804a920f
Grant King <grantryan.king@gmail.com>
2025-01-27 07:43:55 -0800

    Update SponsorOnboardingView.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/views/SponsorOnboardingView.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/views/SponsorOnboardingView.vue
@@ -1,9 +1,15 @@
 <template>
     <div>
+        <BasicHeader eyebrow="THE place to manage all of your sponsorship account" title="Sponsor Dashboard" description="update your profile, add messages, look at all the custom stuff for you here." />
+    </div>
+    <div>
         <OnboardingForm />
     </div>
     <div>
         <OnboardingReview />
+    </div>
+    <div>
+        <OnboardingReviewMessages />
     </div>
     <div>
         <OnboardingCheckout />
@@ -11,8 +17,10 @@
 </template>
 
 <script setup>
+import BasicHeader from '@/components/BasicHeader.vue';
 import OnboardingForm from '@/components/OnboardingForm.vue';
 import OnboardingReview from '@/components/OnboardingReview.vue';
+import OnboardingReviewMessages from '@/components/OnboardingReviewMessages.vue';
 import OnboardingCheckout from '@/components/OnboardingCheckout.vue';
 
 import { useSponsorprofilesStore } from '@/stores/sponsorprofiles';
