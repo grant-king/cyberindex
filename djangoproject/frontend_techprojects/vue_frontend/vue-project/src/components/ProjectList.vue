c12a130b3f9f3da598f41ca4f43a90c6b4c02d14
Grant King <grantryan.king@gmail.com>
2025-01-28 05:18:04 -0800

    Update ProjectList.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/ProjectList.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/ProjectList.vue
@@ -2,13 +2,19 @@
     <div class="bg-white/50 py-24 sm:py-32">
         <div class="mx-auto">
             <div class="bg-black">
-                <BasicHeader eyebrow="Welcome" title="Civic Tech Index Results" description="Where thine shalt discoverish." />
+                <BasicHeader eyebrow="Welcome" title="Civic Tech Index Results"
+                    description="Where thine shalt discoverish." />
             </div>
             <div class="mx-auto mt-32 grid max-w-5xl grid-cols-1 gap-16">
                 <div v-for="project in projects_store.project_list" :key="project.url" class="">
                     <ProjectBrief :project="project" />
                 </div>
             </div>
+        </div>
+        <div :v-show="projects_store.next_page" @click="projects_store.fetchProjects">
+            <button class="mt-16 flex items-center justify-center font-mono w-full h-20 bg-lime-400/40 rounded-lg">
+                LOAD MORE
+            </button>
         </div>
     </div>
 </template>
