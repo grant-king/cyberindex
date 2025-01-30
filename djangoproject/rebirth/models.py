73c46ff648fcd184e58e566d28f3161781fd2997
Grant King <grantryan.king@gmail.com>
2025-01-30 11:12:17 -0800

    Update models.py

--- djangoproject/rebirth/models.py
+++ djangoproject/rebirth/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Avatar(models.Model):
     session_key = models.CharField(max_length=40, db_index=True)
     public_image = models.ImageField(upload_to="avatars/")
-    original_hash = models.CharField(max_length=40, null=True)
-    public_hash = models.CharField(max_length=40, db_index=True)
+    original_hash = models.CharField(max_length=64, null=True)
+    public_hash = models.CharField(max_length=64, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     @property
