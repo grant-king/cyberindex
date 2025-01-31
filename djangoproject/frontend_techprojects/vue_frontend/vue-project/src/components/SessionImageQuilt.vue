bbf00dece3d3d4333e8d8b2c930cf45a9b6c0a26
Grant King <grantryan.king@gmail.com>
2025-01-30 23:16:33 -0800

    Create SessionImageQuilt.vue

---
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/SessionImageQuilt.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-wrap" @click="stats_store.fetchAllImages">
+        <div v-for="(image, idx) in stats_store.all_images" :key="idx" class="">
+            <div class="h-24 w-24">
+                <img class="aspect-square object-cover border-white border-2 border-dotted rounded-md" :src="image" />
+            </div>
+            
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+import { useAvatarStatsStore } from '@/stores/avatarstats'
+import { ref } from 'vue'
+
+const stats_store = useAvatarStatsStore()
+stats_store.fetchAllImages()
+
+</script>
