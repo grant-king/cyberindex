c47fc3a4dce681b652debbd4d2e1769aac3f11ff
Grant King <grantryan.king@gmail.com>
2025-01-31 00:58:36 -0800

    Update SessionImageQuilt.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/SessionImageQuilt.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/SessionImageQuilt.vue
@@ -1,10 +1,15 @@
 <template>
-    <div class="flex flex-wrap" @click="stats_store.fetchAllImages">
-        <div v-for="(image, idx) in stats_store.all_images" :key="idx" class="">
-            <div class="h-24 w-24">
-                <img class="aspect-square object-cover border-white border-2 border-dotted rounded-md" :src="image" />
+    <div class="bg-white/50 p-8" @click="stats_store.fetchAllImages">
+        <div class="text-2xl my-8">
+            Session Image Quilt
+        </div>
+        <div class="flex flex-wrap">
+            <div v-for="(image, idx) in stats_store.all_images" :key="idx" class="">
+                <div class="h-24 w-24">
+                    <img class="aspect-square object-cover border-white border-2 border-dotted rounded-md" :src="image" />
+                </div>
+                
             </div>
-            
         </div>
     </div>
 
@@ -15,6 +20,6 @@ import { useAvatarStatsStore } from '@/stores/avatarstats'
 import { ref } from 'vue'
 
 const stats_store = useAvatarStatsStore()
-stats_store.fetchAllImages()
+//stats_store.fetchAllImages()
 
 </script>
