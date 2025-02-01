6d00d28fb097c0e436a0910e1ee9540f47332675
Grant King <grantryan.king@gmail.com>
2025-01-31 20:23:37 -0800

    Update SessionConsole.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/SessionConsole.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/SessionConsole.vue
@@ -9,40 +9,7 @@
                     session stats: pixeldances, reflections
                     <SessionStats stgrp_idx="0" title="Session Stats" />
                 </div>
-                <div class="mt-8">
-                    Want to connect this session to other sessions?
-                    submit a session image
-                    <div class="my-4">
-                        <input :v-model="image_name" type="file" accept="image/png" @change="handle_file_change" />
-                    </div>
-                    <div v-if="image_name">
-                        <div class="mx-auto max-w-2xl">
-                            <div class="">
-                                <div class="flex justify-center">
-                                    <img class="border-white border-2 border-dotted rounded-md" :src="image_preview" />
-                                </div>
-                            </div>
-                        </div>
-                        <div class="my-4">
-                            <div class="">
-                                <button class="border-dotted border-4 rounded-md bg-clip-border text-black font-bold px-3 py-2"
-                                @click="avatarcreate.createAvatar">
-                                    Submit {{ image_name }}
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    It will be hashed, converted to webp, then uploaded along with the hash for the origninal image
-                    You must submit the original PNG image in order to associate this session with other sessions that
-                    also submitted the original image
-                </div>
-                <div>
-                    The hash is used to link sessions for aggregated stats and to provide a greater level of persistence
-                    for users who don't wan tto lose platform progress.
-                    The converted image is used to build our community session quilt, check it out below.
-                </div>
+                
                 <div>
                     session stats associated with image: ''
                     <SessionStats stgrp_idx="0" title="Linked Session Stats" />
@@ -56,27 +23,6 @@
 
 <script setup>
 import { ref } from 'vue'
-import BasicHeader from './BasicHeader.vue'
 import SessionStats from './SessionStats.vue'
-import { useAvatarCreateStore } from '@/stores/avatarcreate'
-
-const avatarcreate = useAvatarCreateStore()
-
-const temp_img = 'https://via.placeholder.com/150'
-const image_name = ref(null)
-const image_preview = ref(null)
-
-async function handle_file_change(event) {
-    const file = event.target.files[0]
-    image_name.value = file.name
-    //await avatarcreate.convertImageToWebP()
-    const reader = new FileReader()
-    reader.onload = (e) => {
-        console.log(e.target.result)
-        image_preview.value = e.target.result
-    }
-    reader.readAsDataURL(file)
-    avatarcreate.original_image_file = file
-}
 
 </script>
