7e379b9c978a09991c9e0d3d7a4c29b6dc3d2172
Grant King <grantryan.king@gmail.com>
2025-02-01 08:21:26 -0800

    Update HomeView.vue

--- djangoproject/buildforge/vue_frontend/vue-project/src/views/HomeView.vue
+++ djangoproject/buildforge/vue_frontend/vue-project/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <MainMenu />
+  </div>
 </template>
+
+<script setup>
+import MainMenu from '@/components/MainMenu.vue';
+</script>
