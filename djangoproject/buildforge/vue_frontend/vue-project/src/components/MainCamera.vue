ade0fa96573a9e0d344368faf4a43c237d8ee338
Grant King <grantryan.king@gmail.com>
2025-02-01 11:29:31 -0800

    Create MainCamera.vue

---
+++ djangoproject/buildforge/vue_frontend/vue-project/src/components/MainCamera.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script setup>
+import { useCameraStore } from '@/stores/camera';
+import { onMounted } from 'vue';
+
+const camera_store = useCameraStore()
+
+onMounted(() => {
+    camera_store.createCamera()
+})
+</script>
