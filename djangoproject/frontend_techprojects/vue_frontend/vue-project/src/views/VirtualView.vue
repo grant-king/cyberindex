6c374f77ea8a2588a1453125a569c14d8b7dba11
Grant King <grantryan.king@gmail.com>
2025-02-01 05:59:57 -0800

    Create VirtualView.vue

---
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/views/VirtualView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="min-h-screen bg-white/10">
+        <AFrameDemo />
+    </div>
+</template>
+
+<script setup>
+import AFrameDemo from '@/components/AFrameDemo.vue'
+
+</script>
