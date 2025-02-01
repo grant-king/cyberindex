565c2766689aa729a61c61aa49c72d09dbbedde8
Grant King <grantryan.king@gmail.com>
2025-02-01 06:26:21 -0800

    Update admin.py

--- djangoproject/cashier/admin.py
+++ djangoproject/cashier/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Order, StripePayment, FundsCredit, FundsDebit
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(StripePayment)
+admin.site.register(FundsCredit)
+admin.site.register(FundsDebit)
