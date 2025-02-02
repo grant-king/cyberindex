958e7d200f376983550f762222409513a60d0377
Grant King <grantryan.king@gmail.com>
2025-02-01 22:28:49 -0800

    Create MainScene.vue

---
+++ djangoproject/buildforge/vue_frontend/vue-project/src/components/MainScene.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useSceneStore } from '@/stores/scene';
+
+const scene_store = useSceneStore()
+
+onMounted(() => {
+    //scene_store.initScene()
+})
+
+</script>
