125f2236d909f9943ed02df02805b0b8a9880852
Grant King <grantryan.king@gmail.com>
2025-02-02 01:01:28 -0800

    Update MainScene.vue

--- djangoproject/buildforge/vue_frontend/vue-project/src/components/MainScene.vue
+++ djangoproject/buildforge/vue_frontend/vue-project/src/components/MainScene.vue
@@ -6,23 +6,54 @@
 import { ref, onMounted } from 'vue';
 import { useSceneStore } from '@/stores/scene';
 import { useVoxelStore } from '@/stores/voxel';
+import { useLightStore } from '@/stores/light';
 
 const scene_store = useSceneStore()
+const light_store = useLightStore()
+const voxel_store = useVoxelStore()
 
 onMounted(() => {
-    build_scene()
+    createSun()
+    createLand()
+    createTower()
+    buildScene()
 })
 
-function build_scene() {
+function createSun() {
+    light_store.createLight(4, 8, 16)
+    light_store.createLight(-4, 8, 16)
+}
+
+function createLand() {
     const voxel_store = useVoxelStore()
     for (let i = -16; i < 16; i++) {
-        for (let j = -16; j < 16; j++) {
-            for (let k = 0; k < 1; k++) {
+        for (let j = -1; j < 0; j++) {
+            for (let k = -16; k < 16; k++) {
                 voxel_store.createVoxel(i, j, k)
             }
         }
     }
-    for (let voxel of voxel_store.voxel_list){
+}
+
+function createTower() {
+    const voxel_store = useVoxelStore()
+    for (let i = -2; i < 2; i++) {
+        for (let j = 0; j < 20; j++) {
+            for (let k = -2; k < 2; k++) {
+                voxel_store.createVoxel(i, j, k)
+            }
+        }
+    }
+}
+
+function buildScene() {
+    // add lights
+    for (let light of light_store.light_list) {
+        const light_copy = light.clone()
+        scene_store.add(light_copy)
+    }
+    // add voxels
+    for (let voxel of voxel_store.voxel_list) {
         const voxel_copy = voxel.clone()
         scene_store.add(voxel_copy)
     }
