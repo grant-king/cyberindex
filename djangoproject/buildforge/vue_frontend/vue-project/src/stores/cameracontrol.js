4913334a99d9ebe644b07191c01f29dfb8b9d657
Grant King <grantryan.king@gmail.com>
2025-02-02 12:05:03 -0800

    Update cameracontrol.js

--- djangoproject/buildforge/vue_frontend/vue-project/src/stores/cameracontrol.js
+++ djangoproject/buildforge/vue_frontend/vue-project/src/stores/cameracontrol.js
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import * as THREE from 'three'
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js'
 import { useCameraStore } from './camera.js'
 
 const camera_store = useCameraStore()
@@ -10,6 +11,15 @@ export const useCameracontrolStore = defineStore('cameracontrol', () => {
   const position = ref(new THREE.Vector3())
   const velocity = ref(new THREE.Vector3())
   const acceleration = ref(new THREE.Vector3())
+  const pointer_controls = ref(null)
+
+  function initPointerControls() {
+    pointer_controls.value = new PointerLockControls(camera_store.createCamera(), document.body)
+    camera_store.camera = pointer_controls.value.object
+    document.body.addEventListener('click', function() {
+      pointer_controls.value.lock()
+    })
+  }
 
   function updateCameraPosition(delta_time) {
     velocity.value.add(acceleration.value.clone().multiplyScalar(delta_time))
@@ -19,5 +29,5 @@ export const useCameracontrolStore = defineStore('cameracontrol', () => {
    
   
 
-  return { updateCameraPosition  }
+  return { updateCameraPosition, initPointerControls, pointer_controls }
 })
