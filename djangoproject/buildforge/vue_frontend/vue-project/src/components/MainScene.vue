c6f214662cbdbf293a2c22fd61b4ad45d8f0dc79
Grant King <grantryan.king@gmail.com>
2025-02-01 22:43:02 -0800

    Update MainScene.vue

--- djangoproject/buildforge/vue_frontend/vue-project/src/components/MainScene.vue
+++ djangoproject/buildforge/vue_frontend/vue-project/src/components/MainScene.vue
@@ -5,11 +5,29 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useSceneStore } from '@/stores/scene';
+import { useVoxelStore } from '@/stores/voxel';
 
 const scene_store = useSceneStore()
 
 onMounted(() => {
-    //scene_store.initScene()
+    build_scene()
 })
+
+function build_scene() {
+    const voxel_store = useVoxelStore()
+    for (let i = -16; i < 16; i++) {
+        for (let j = -16; j < 16; j++) {
+            for (let k = 0; k < 1; k++) {
+                voxel_store.createVoxel(i, j, k)
+            }
+        }
+    }
+    for (let voxel of voxel_store.voxel_list){
+        const voxel_copy = voxel.clone()
+        scene_store.add(voxel_copy)
+    }
+}
+
+
 
 </script>
