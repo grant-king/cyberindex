bbe4fb6bbff6c3664aaf4f19b037f95fb0b94fa6
Grant King <grantryan.king@gmail.com>
2025-02-01 22:28:42 -0800

    Create voxel.js

---
+++ djangoproject/buildforge/vue_frontend/vue-project/src/stores/voxel.js
@@ -0,0 +1,21 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import * as THREE from 'three'
+
+export const useVoxelStore = defineStore('voxel', () => {
+  const voxel_list = ref([])
+
+  function createVoxel(x, y, z) {
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const color = Math.random() * 0xffffff
+    const material = new THREE.MeshBasicMaterial({ color: color })
+    const voxel = new THREE.Mesh(geometry, material)
+    voxel.position.set(x, y, z)
+    voxel_list.value.push(voxel)
+    //console.log('voxel_list', voxel_list.value)
+    return voxel
+  }
+
+
+  return { createVoxel, voxel_list }
+})
