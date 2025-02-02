67b811446c384f044fe3b18ed68bd5f27cd266a2
Grant King <grantryan.king@gmail.com>
2025-02-02 14:03:03 -0800

    Update MainCamera.vue

--- djangoproject/buildforge/vue_frontend/vue-project/src/components/MainCamera.vue
+++ djangoproject/buildforge/vue_frontend/vue-project/src/components/MainCamera.vue
@@ -6,37 +6,8 @@
 </template>
 
 <script setup>
-import { useCameraStore } from '@/stores/camera';
-import { onMounted } from 'vue';
-import { ref, watch } from 'vue';
-
-const camera_store = useCameraStore()
-
-onMounted(() => {
-    camera_store.createCamera()
-    window.addEventListener('keydown', handleCameraControls)
-})
-
-function handleCameraControls(event) {
-    if (event.key === 'a' || event.key === 'ArrowLeft') {
-        camera_store.updatePosition(-1, 0, 0)
-    }
-    if (event.key === 'd' || event.key === 'ArrowRight') {
-        camera_store.updatePosition(1, 0, 0)
-    }
-    if (event.key === 'e' || event.key === 'Shift') {
-        camera_store.updatePosition(0, 1, 0)
-    }
-    if (event.key === 'q' || event.key === 'Control') {
-        camera_store.updatePosition(0, -1, 0)
-    }
-    if (event.key === 'w' || event.key === 'ArrowUp') {
-        camera_store.updatePosition(0, 0, -1)
-    }
-    if (event.key === 's' || event.key === 'ArrowDown') {
-        camera_store.updatePosition(0, 0, 1)
-    }
-}
-
+import { useCameraStore } from '@/stores/camera'
+import { onMounted } from 'vue'
+import { ref, watch } from 'vue'
 
 </script>
