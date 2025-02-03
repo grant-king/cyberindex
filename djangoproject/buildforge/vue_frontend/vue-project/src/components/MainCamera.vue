93bb2056902cb1e4881224c53fb6d6fc2efa3b84
Grant King <grantryan.king@gmail.com>
2025-02-03 12:15:30 -0800

    Update MainCamera.vue

--- djangoproject/buildforge/vue_frontend/vue-project/src/components/MainCamera.vue
+++ djangoproject/buildforge/vue_frontend/vue-project/src/components/MainCamera.vue
@@ -7,7 +7,32 @@
 
 <script setup>
 import { useCameraStore } from '@/stores/camera'
-import { onMounted } from 'vue'
+import { useCollectorStore } from '@/stores/collector'
+import { useSceneStore } from '@/stores/scene'
+import { useVoxelStore } from '@/stores/voxel'
+
+import { onMounted, onUnmounted } from 'vue'
 import { ref, watch } from 'vue'
+
+const camera_store = useCameraStore()
+const collector_store = useCollectorStore()
+const scene_store = useSceneStore()
+const voxel_store = useVoxelStore()
+
+onMounted(() => {
+    setInterval(measureCollector, 2000)
+})
+
+onUnmounted(() => {
+    clearInterval(measureCollector)
+})
+
+function measureCollector() {
+    const cam_x = camera_store.camera.position.x
+    const cam_y = camera_store.camera.position.y
+    const cam_z = camera_store.camera.position.z
+    collector_store.getCollectiblesInRegion(cam_x, cam_y, cam_z)
+    console.log(collector_store.collection_queue)
+}
 
 </script>
