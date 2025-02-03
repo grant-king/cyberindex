2594c0cfcc854de3daa312bbe0d109d14be7304b
Grant King <grantryan.king@gmail.com>
2025-02-03 07:12:26 -0800

    Update voxel.js

--- djangoproject/buildforge/vue_frontend/vue-project/src/stores/voxel.js
+++ djangoproject/buildforge/vue_frontend/vue-project/src/stores/voxel.js
@@ -1,21 +1,64 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import * as THREE from 'three'
+import alphaMapUrl from '@/assets/alpha_map.png'
 
 export const useVoxelStore = defineStore('voxel', () => {
   const voxel_list = ref([])
+  const voxel_mesh_list = ref([])
+  const base_url = window.location.origin
+  const endpoint = `${base_url}/apiv1/voxels/`
+  const next_page = ref(`${endpoint}?page=1`)
 
-  function createVoxel(x, y, z) {
+  async function drawVoxels() {
+    for (const voxel of voxel_list.value) {
+      drawVoxel(voxel.x, voxel.y, voxel.z)
+    }
+  }
+
+  function drawVoxel(x, y, z) {
     const geometry = new THREE.BoxGeometry(1, 1, 1)
     const color = Math.random() * 0xffffff
-    const material = new THREE.MeshPhongMaterial({ color: color })
+    //const alpha_map = new THREE.TextureLoader().load(alphaMapUrl)
+    //const material = new THREE.MeshPhongMaterial({ color: color, alphaMap: alpha_map, combine: THREE.MixOperation, transparent: true })
+    const material = new THREE.MeshPhongMaterial({ color: color})
     const voxel = new THREE.Mesh(geometry, material)
     voxel.position.set(x, y, z)
-    voxel_list.value.push(voxel)
-    //console.log('voxel_list', voxel_list.value)
+    // add a very slight random rotation
+    voxel.rotation.set(
+      Math.random() * 0.01,
+      Math.random() * 0.01,
+      Math.random() * 0.01
+    )
+    
+    voxel_mesh_list.value.push(voxel)
+    console.log('voxel mesh:', voxel)
     return voxel
   }
 
+  async function fetchVoxels() {
+    if (next_page.value === null) {
+      console.log('no more pages to fetch')
+      return
+    }
+    const response = await fetch(next_page.value, {
+      method: 'GET',
+      headers: {
+        'X-CSRFToken': window.csrf_token,
+      },
+    })
+    const data = await response.json()
+    if (response.ok) {
+      console.log('voxels', data)
+      voxel_list.value.push(...data.results)
+      next_page.value = data.next
+    } else {
+      console.error('failed to fetch voxels')
+      console.error(response)
+    }
+  }
 
-  return { createVoxel, voxel_list }
+
+
+  return { voxel_mesh_list, drawVoxels, fetchVoxels }
 })
