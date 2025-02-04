9f7004a8dc3cc4deee7e0b55d6e11e4c46ad432d
Grant King <grantryan.king@gmail.com>
2025-02-04 05:24:36 -0800

    Update views_api.py

--- djangoproject/buildforge/views_api.py
+++ djangoproject/buildforge/views_api.py
@@ -16,9 +16,9 @@ from buildforge.serializers import (
 )
 
 class LargeResultsSetPagination(PageNumberPagination):
-    page_size = 1000
+    page_size = 2000
     page_size_query_param = 'page_size'
-    max_page_size = 1000
+    max_page_size = 10000
 
 
 class VoxelViewSet(viewsets.ModelViewSet):
@@ -36,11 +36,11 @@ class VoxelViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['get']) # add 8x1xN plane of voxels
     def plane(self, request):
-        for x in range(-8, 8):
-            for y in range(1):
+        for x in range(-16, 16):
+            for y in range(-2, -1):
                 for z in range(-32, -8):
                     random_color = "%06x" % random.randint(0, 0xFFFFFF)
-                    #Voxel.objects.create(x=x, y=y, z=z, color=random_color)
+                    Voxel.objects.create(x=x, y=y, z=z, color=random_color)
         return Response(status=status.HTTP_201_CREATED)
     
     @action(detail=False, methods=['get']) # add 1xNx1 tower of voxels
