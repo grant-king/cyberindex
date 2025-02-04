b8cf2b51fddf588921094bd7710f0575c12b0821
Grant King <grantryan.king@gmail.com>
2025-02-04 03:24:28 -0800

    Update MainRender.vue

--- djangoproject/buildforge/vue_frontend/vue-project/src/components/MainRender.vue
+++ djangoproject/buildforge/vue_frontend/vue-project/src/components/MainRender.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { onMounted } from 'vue'
 import { useCameraStore } from '@/stores/camera'
 import { useSceneStore } from '@/stores/scene'
@@ -19,29 +19,36 @@ const scene_store = useSceneStore()
 const cameracontrol_store = useCameracontrolStore()
 const collector_store = useCollectorStore()
 const clock = new THREE.Clock()
+let renderer = new THREE.WebGLRenderer()
 
 onMounted(() => {
     scene_store.initScene()
     main()
 })
 
+function render(start_time) {
+    if (cameracontrol_store.pointer_controls.isLocked === true) {
+        cameracontrol_store.updateCameraPosition(clock.getDelta())
+    }
+    
+    renderer.render(scene_store.render_scene, camera_store.camera)
+    requestAnimationFrame(render)
+    
+}
 function main() {
     const canvas = document.querySelector('#three-canvas')
-    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
+    renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
     renderer.setSize(window.innerWidth, window.innerHeight)
 
     cameracontrol_store.initControls() // can move to MainCamera.vue
 
-    function render(start_time) {
-        if (cameracontrol_store.pointer_controls.isLocked === true) {
-            cameracontrol_store.updateCameraPosition(clock.getDelta())
-        }
-
-        renderer.render(scene_store.render_scene, camera_store.camera)
-        requestAnimationFrame(render)
-
-    }
-
     requestAnimationFrame(render)
+
 }
+//watch(() => scene_store.current_scene, () => {
+//    console.log('scene changed')
+//    renderer.render(scene_store.render_scene, camera_store.camera)
+//    requestAnimationFrame(render)
+
+//})
 </script>
