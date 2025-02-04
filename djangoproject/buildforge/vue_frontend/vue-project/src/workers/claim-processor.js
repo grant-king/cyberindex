d37fad995e4f3b22dc25bcdae40e5a96941bc745
Grant King <grantryan.king@gmail.com>
2025-02-04 08:24:28 -0800

    Update claim-processor.js

--- djangoproject/buildforge/vue_frontend/vue-project/src/workers/claim-processor.js
+++ djangoproject/buildforge/vue_frontend/vue-project/src/workers/claim-processor.js
@@ -1,12 +1,32 @@
 onmessage = (e) => {
-    const claim  = e.data;
-    const result = processClaim(claim);
+    const [voxel_pk, endpoint, token]  = e.data;
+    const result = processClaim(voxel_pk, endpoint, token);
     postMessage(result);
     };
 
-function processClaim(claim) {
-    console.log('worker processing claim:', claim)
-    //const { voxel_pk } = claim
-    //createClaim(voxel_pk)
+function processClaim(voxel_pk, endpoint, token) {
+    console.log('worker processing claim:', voxel_pk, endpoint, token)
+    const claim = createClaim(voxel_pk, endpoint, token)
     return 'claim processed by worker'
+}
+
+async function createClaim(voxel_pk, endpoint, token) {
+    const form_data = new FormData()
+    form_data.append('voxel', voxel_pk)
+    form_data.append('is_holding', true)
+    const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+            'X-CSRFToken': token,
+        },
+        body: form_data,
+    })
+    const data = await response.json()
+    if (response.ok) {
+        console.log('created claim', data)
+        return data
+    } else {
+        console.error('failed to create claim')
+        console.error(response)
+    }
 }
