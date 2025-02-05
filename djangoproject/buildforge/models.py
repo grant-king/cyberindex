1c9dddb6f98758585c9832547844cb60f0490033
Grant King <grantryan.king@gmail.com>
2025-02-05 15:27:24 -0800

    Update models.py

--- djangoproject/buildforge/models.py
+++ djangoproject/buildforge/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Voxel(models.Model):
-    color = models.CharField(max_length=6)
+    color = models.CharField(max_length=6, blank=True)
     x = models.IntegerField(db_index=True)
     y = models.IntegerField(db_index=True)
     z = models.IntegerField(db_index=True)
