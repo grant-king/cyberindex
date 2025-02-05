42358c6c979a4b3bb4a39709074fde439c83dbb1
Grant King <grantryan.king@gmail.com>
2025-02-05 03:20:30 -0800

    Update views_api.py

--- djangoproject/buildforge/views_api.py
+++ djangoproject/buildforge/views_api.py
@@ -85,11 +85,11 @@ class VoxelViewSet(viewsets.ModelViewSet):
         for x in range(-8, 8):
             for y in range(-8, 8):
                 for z in range(-8, 8):
-                    if x ** 2 + y ** 2 + z ** 2 < 8 ** 2:
-                        #make hollow
-                        if x ** 2 + y ** 2 + z ** 2 > 6 ** 2:
+                    if x**2 + y**2 + z**2 < 8**2:
+                        # make hollow
+                        if x**2 + y**2 + z**2 > 6**2:
                             random_color = "%06x" % random.randint(0, 0xFFFFFF)
-                            #Voxel.objects.create(x=x, y=y, z=z, color=random_color)
+                            # Voxel.objects.create(x=x, y=y, z=z, color=random_color)
         return Response(status=status.HTTP_201_CREATED)
 
     @action(detail=False, methods=["get"])  # get narest 256 voxels to a point
@@ -99,9 +99,9 @@ class VoxelViewSet(viewsets.ModelViewSet):
         z = int(request.query_params.get("z", 0))
         voxel_coord_list = []
         # build list of coords for nearest 256 voxels
-        for x_friend in range(x-8, x+8):
-            for y_friend in range(y-8, y+8):
-                for z_friend in range(z-8, z+8):
+        for x_friend in range(x - 8, x + 8):
+            for y_friend in range(y - 8, y + 8):
+                for z_friend in range(z - 8, z + 8):
                     voxel_coord_list.append((x_friend, y_friend, z_friend))
         voxels = Voxel.objects.filter(
             x__in=[x for x, y, z in voxel_coord_list],
@@ -110,6 +110,7 @@ class VoxelViewSet(viewsets.ModelViewSet):
         )
         serializer = VoxelSerializer(voxels, many=True)
         return Response(serializer.data)
+
 
 class ClaimViewSet(viewsets.ModelViewSet):
     queryset = Claim.objects.all()
@@ -123,9 +124,27 @@ class ClaimViewSet(viewsets.ModelViewSet):
         Claim.objects.all().delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 class BuilderViewSet(viewsets.ModelViewSet):
     queryset = Builder.objects.all()
     serializer_class = BuilderSerializer
 
     def perform_create(self, serializer):
         serializer.save(session_key=self.request.session.session_key)
+
+    @action(detail=False, methods=["get"])
+    def my_builder(self, request):
+        builder = Builder.objects.filter(session_key=self.request.session.session_key)
+        serializer = BuilderSerializer(builder, many=True)
+        return Response(serializer.data)
+
+
+class PlacementPromiseViewSet(viewsets.ModelViewSet):
+    queryset = PlacementPromise.objects.all()
+    serializer_class = PlacementPromiseSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(session_key=self.request.session.session_key)
+
+    def get_queryset(self):
+        return super().get_queryset().filter(session_key=self.request.session.session_key)
