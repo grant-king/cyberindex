9c655e63a143d85042848c255b8481c82aebeb86
Grant King <grantryan.king@gmail.com>
2025-02-05 03:21:15 -0800

    Update index.js

--- djangoproject/buildforge/vue_frontend/vue-project/src/router/index.js
+++ djangoproject/buildforge/vue_frontend/vue-project/src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/GameView.vue'),
     },
+    {
+      path: '/build',
+      name: 'build',
+      component: () => import('../views/BuildView.vue'),
+    },
   ],
 })
 
