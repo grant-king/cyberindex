5a5f7f6c6fdddc54e8bbd9e93299b6630eb96efd
Grant King <grantryan.king@gmail.com>
2025-02-05 04:53:52 -0800

    Update BuildView.vue

--- djangoproject/buildforge/vue_frontend/vue-project/src/views/BuildView.vue
+++ djangoproject/buildforge/vue_frontend/vue-project/src/views/BuildView.vue
@@ -1,7 +1,23 @@
 <template>
-    BUILD VIEW
+    <div class="">
+        <div class="bg-black/50 overflow-auto max-w-4xl mx-auto p-4">
+            <div class="grid grid-cols-8 gap-4">
+                <div v-for="slot in grid_slots" :key="slot" class="column">
+                    <div class="aspect-square w-auto bg-gray-200 border border-gray-300">
+                        {{ slot }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
+import { useCollectorStore } from '@/stores/collector';
+
+const collector_store = useCollectorStore()
+collector_store.fetchClaims()
+
+const grid_slots = ref(Array.from({ length: 64 }, (_, idx) => idx))
 </script>
