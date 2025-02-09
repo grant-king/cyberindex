d43922bba0f06e9c7057be68262e84d012f88af7
Grant King <grantryan.king@gmail.com>
2025-02-09 01:04:31 -0800

    Update models.py

--- djangoproject/techprojects/models.py
+++ djangoproject/techprojects/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from .managers import ProjectRegistrationManager
 
+
 class Location(models.Model):
     city = models.CharField(max_length=100, db_index=True)
     state = models.CharField(max_length=100, blank=True, null=True)
     country = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.city}, {self.state if self.state is not None else ''}, {self.country}'
+        return f"{self.city}, {self.state if self.state is not None else ''}, {self.country}"
 
 
 class ProjectRegistration(models.Model):
@@ -16,22 +17,33 @@ class ProjectRegistration(models.Model):
     submission_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     image_url = models.URLField(null=True, blank=True)
-    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True, related_name='projects')
-    repo_details = models.JSONField(null=True, blank=True) # response from github api containing additional details about the repository like number of stars, forks, etc.
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="projects",
+    )
+    repo_details = models.JSONField(
+        null=True, blank=True
+    )  # response from github api containing additional details about the repository like number of stars, forks, etc.
     objects = ProjectRegistrationManager()
 
     def __str__(self):
         return self.name
-    
-    class Meta:
-        ordering = ['-last_updated']
-    
 
-class ProjectResourceLink(models.Model): # additional links to resources like documentation, demo, etc.
-    project = models.ForeignKey(ProjectRegistration, on_delete=models.CASCADE, related_name='resource_links')
+    class Meta:
+        ordering = ["-last_updated"]
+
+
+class ProjectResourceLink(
+    models.Model
+):  # additional links to resources like documentation, demo, etc.
+    project = models.ForeignKey(
+        ProjectRegistration, on_delete=models.CASCADE, related_name="resource_links"
+    )
     link = models.URLField()
     description = models.TextField()
 
     def __str__(self):
         return self.link
-    
