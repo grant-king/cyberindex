d96f5c156adbbc20c84dfda2aa8bd82cc646a956
Grant King <grantryan.king@gmail.com>
2025-02-10 03:35:39 -0800

    Update Login.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/Login.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/Login.vue
@@ -56,7 +56,7 @@
   const base_url = window.location.origin
   const reset_url = `${base_url}/password_reset/`
   
-  const company_logo_url = 'https://unrolla.blob.core.windows.net/unrolla/images/cti_logo.webp'
+  const company_logo_url = 'https://myctpmedia.blob.core.windows.net/default/avatars/f8d909803de8b75b0972436093b8e5be67d3c2d3f8a09106674398dd6df98b37.webp'
   
   const username = ref('')
   const password = ref('')
