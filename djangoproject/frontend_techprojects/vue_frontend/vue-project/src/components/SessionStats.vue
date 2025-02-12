bcab9af84eec3338848925154c6e16752e4229ba
Grant King <grantryan.king@gmail.com>
2025-02-11 16:59:27 -0800

    Update SessionStats.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/SessionStats.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/SessionStats.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="bg-white/10 rounded-lg py-24 sm:py-32">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="bg-white/10 rounded-lg py-8">
+        <div class="mx-auto max-w-7xl px-8">
             <div class="mx-auto max-w-2xl lg:max-w-none" @click="stats_store.fetchSessionStats">
                 <div class="font-mono">
                     <h2 class="text-balance text-2xl font-semibold tracking-tight text-black">{{ title }}</h2>
                     <p class="mt-4 text-lg/8 text-black/80">{{ subheading }}</p>
                 </div>
                 <dl
-                    class="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
+                    class="mt-4 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
                     <div v-for="(stat, value) in stats_store.session_stats" :key="stat" class="flex flex-col bg-white/5 p-8">
                         <dt class="text-sm/6 font-semibold text-black/80">{{ value }}</dt>
                         <dd class="order-first text-3xl font-semibold tracking-tight text-black">{{ stat }}
