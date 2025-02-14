8d2b8aaf36bce54313935bc906c2eb54fbb2e2d3
Grant King <grantryan.king@gmail.com>
2025-02-13 17:27:25 -0800

    Update WebGPUDemo.vue

--- djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/WebGPUDemo.vue
+++ djangoproject/frontend_techprojects/vue_frontend/vue-project/src/components/WebGPUDemo.vue
@@ -39,11 +39,23 @@ onMounted(async () => {
         colorAttachments: [{
             view: context.value.getCurrentTexture().createView(),
             loadOp: 'clear',
+            clearValue: { r: 0.0, g: 0.2, b: 0.0, a: 0.2 },
             storeOp: 'store'
         }]
     })
-
+    // end the render pass
+    pass.end()
+    // create a handle to the recorded commands
+    const command_buffer = encoder.value.finish()
+    // submit the command buffer to the GPU's queue, takes a list of command buffers
+    // once submitted, buffer can't be used again. build another command buffer to submit more commands
+    
+    device.value.queue.submit([command_buffer])
+    // can also be one-liner
+    // device.queue.submit([encoder.finish()])
 })
+
+
 
 
 </script>
