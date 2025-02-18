47fe6e8c92fffba077db885d4f053a73f10bd87e
Grant King <grantryan.king@gmail.com>
2025-02-17 17:51:31 -0800

    Update buildforge static
    to mark redeploy

--- djangoproject/buildforge/static/buildforge/assets/voxel-Dld7Trh8.js
+++ djangoproject/buildforge/static/buildforge/assets/voxel-Dld7Trh8.js
@@ -1,4 +1,4 @@
-import{d as ti,r as ft,j as Qi,k as Ki}from"./index-BEuX0Jok.js";/**
+import{d as ti,r as ft,j as Qi,k as Ki}from"./index-DwzpUYfM.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
