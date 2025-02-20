6be1778cea2cb4c4b5b436b9c4933866ce21755a
Grant King <grantryan.king@gmail.com>
2025-02-20 12:09:59 -0800

    Update BuilderPosGizmo.vue
    Wire up builder position buttons to builder store.

--- djangoproject/buildforge/vue_frontend/vue-project/src/components/BuilderPosGizmo.vue
+++ djangoproject/buildforge/vue_frontend/vue-project/src/components/BuilderPosGizmo.vue
@@ -2,61 +2,125 @@
     <div class="">
         <div class="bg-white/10 hover:bg-white/20 rounded-sm">
             <div class="grid grid-cols-3 gap-1 text-center place-content-around w-64 h-64">
-                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
-                    
-                        X-&Y+
-                    
-                </div>
-                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+                <button class="size-16 bg-white/10 hover:bg-white/20 rounded-sm " @click="dec_x_inc_y">
 
-                    Y++
+                    X-&Y+
 
-                </div>
-                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+                </button>
+                <button class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center" @click="increment_y">
+
+                    Y+
+
+                </button>
+                <button class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center" @click="increment_x_y">
 
                     X+&Y+
 
-                </div>
-                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+                </button>
+                <button class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center" @click="decrement_x">
 
-                    X--
+                    X-
 
-                </div>
-                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
-                    <div>
-                        Z++
-                    </div>
+                </button>
+                <div class="size-16 place-content-center">
+                    <button class="h-8 w-16 rounded-t-sm bg-white/10 hover:bg-white/20" @click="increment_z">
+                        Z+
+                    </button>
                     <hr>
                     <hr>
-                    <div>
-                        Z--
-                    </div>
+                    <button class="h-8 w-16 bg-white/10 rounded-b-sm hover:bg-white/20" @click="decrement_z">
+                        Z-
+                    </button>
                 </div>
-                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+                <button class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center" @click="increment_x">
 
-                    X++
+                    X+
 
-                </div>
-                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+                </button>
+                <button class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center" @click="decrement_x_y">
 
                     X-&Y-
 
-                </div>
-                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+                </button>
+                <button class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center" @click="decrement_y">
 
-                    Y--
+                    Y-
 
-                </div>
-                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+                </button>
+                <button class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center" @click="inc_x_dec_y">
 
                     X+&Y-
 
-                </div>
+                </button>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import { useBuilderStore } from '@/stores/builder'
+
+const builder_store = useBuilderStore()
+
+onMounted(async () => {
+    await builder_store.fetchMyBuilder()
+})
+
+async function increment_x() {
+    builder_store.my_builder.x++
+    await builder_store.updateBuilder()
+}
+
+async function decrement_x() {
+    builder_store.my_builder.x--
+    await builder_store.updateBuilder()
+}
+
+async function increment_y() {
+    builder_store.my_builder.y++
+    await builder_store.updateBuilder()
+}
+
+async function decrement_y() {
+    builder_store.my_builder.y--
+    await builder_store.updateBuilder()
+}
+
+async function increment_z() {
+    builder_store.my_builder.z++
+    await builder_store.updateBuilder()
+}
+
+
+async function decrement_z() {
+    builder_store.my_builder.z--
+    await builder_store.updateBuilder()
+}
+
+async function increment_x_y() {
+    builder_store.my_builder.x++
+    builder_store.my_builder.y++
+    await builder_store.updateBuilder()
+}
+
+async function decrement_x_y() {
+    builder_store.my_builder.x--
+    builder_store.my_builder.y--
+    await builder_store.updateBuilder()
+}
+
+async function inc_x_dec_y() {
+    builder_store.my_builder.x++
+    builder_store.my_builder.y--
+    await builder_store.updateBuilder()
+}
+
+async function dec_x_inc_y() {
+    builder_store.my_builder.x--
+    builder_store.my_builder.y++
+    await builder_store.updateBuilder()
+}
+
 
 </script>
