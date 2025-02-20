0d888c67609f5415fcdbb62e0d83d2e224f95ef0
Grant King <grantryan.king@gmail.com>
2025-02-20 11:15:35 -0800

    Create BuilderPosGizmo.vue
    Basic 3x3 button grid layout that will make for easy tapping/clicking to navigate the builder.

---
+++ djangoproject/buildforge/vue_frontend/vue-project/src/components/BuilderPosGizmo.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="">
+        <div class="bg-white/10 hover:bg-white/20 rounded-sm">
+            <div class="grid grid-cols-3 gap-1 text-center place-content-around w-64 h-64">
+                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+                    
+                        X-&Y+
+                    
+                </div>
+                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+
+                    Y++
+
+                </div>
+                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+
+                    X+&Y+
+
+                </div>
+                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+
+                    X--
+
+                </div>
+                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+                    <div>
+                        Z++
+                    </div>
+                    <hr>
+                    <hr>
+                    <div>
+                        Z--
+                    </div>
+                </div>
+                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+
+                    X++
+
+                </div>
+                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+
+                    X-&Y-
+
+                </div>
+                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+
+                    Y--
+
+                </div>
+                <div class="size-16 bg-white/10 hover:bg-white/20 rounded-sm place-content-center">
+
+                    X+&Y-
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
