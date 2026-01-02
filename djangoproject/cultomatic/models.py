20e19c8c62da7981021029ccfae7028ad15e465e
Grant King <grantryan.king@gmail.com>
2026-01-01 20:00:54 -0800

    Add Creator and Animation models
    Introduces Creator and Animation models to support user-linked animation uploads. Includes initial migration for database schema setup.

--- djangoproject/cultomatic/models.py
+++ djangoproject/cultomatic/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Creator(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+# model for storing spritesheet animation images
+class Animation(models.Model):
+    creator = models.ForeignKey(Creator, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="cultomatic/animations/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    info = models.JSONField(default=dict) #image and frame size, palette, etc
+
